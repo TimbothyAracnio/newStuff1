80796e4b19af95e4883899131e1529a0d6eb2118
Jeremiah Brints <timbothyaracnio@gmail.com>
2019-10-14 15:34:28 -0500

    made progress

--- MAYTH.py
+++ MAYTH.py
@@ -1,1 +1,28 @@
-# taco cat
+# we are going to look at the approximations of Pi
+# as well as looking at the math Module
+
+print(22/7)
+print(355/113)
+
+import math
+
+print(9801 / (2206 * math.sqrt(2)))
+
+# archimedes Pi:
+
+
+def archimedes(numSides):
+    innerAngleB = 360.0 / numSides
+    halfAngleA = innerAngleB / 2
+    oneHalfSideS = math.sin(math.radians(halfAngleA))
+    sideS = oneHalfSideS * 2
+    polygonCircumfrence = numSides * sideS
+    pi = polygonCircumfrence / 2
+    return pi
+
+print(archimedes(4))
+print(archimedes(8))
+print(archimedes(16))
+
+for sides in range(8, 100, 8):
+    print(sides, archimedes(sides))
