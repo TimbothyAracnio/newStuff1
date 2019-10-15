011a9307bbfdeaafe3bbc745404c1573da921bfe
Jeremiah Brints <timbothyaracnio@gmail.com>
2019-10-15 15:25:43 -0500

    this math is cool! (added math)

--- MAYTH.py
+++ MAYTH.py
@@ -17,12 +17,28 @@ def archimedes(numSides):
     oneHalfSideS = math.sin(math.radians(halfAngleA))
     sideS = oneHalfSideS * 2
     polygonCircumfrence = numSides * sideS
-    pi = polygonCircumfrence / 2
-    return pi
+    pie = polygonCircumfrence / 2
+    return pie
 
 print(archimedes(4))
 print(archimedes(8))
 print(archimedes(16))
 
-for sides in range(8, 100, 8):
-    print(sides, archimedes(sides))
+for sides in range(8, 1000, 8):
+    print(sides, (archimedes(sides) - math.pi))
+
+# See the loop above.  in addition to the value of pi, print the difference
+# between the values calculated by the archimedes function and by math.pi.
+# How many sides does it take to make the two close?
+
+
+
+# Accumulators
+# THIS IS NOT ALGEBRA
+
+acc = 0
+
+for val in range(1, 6):
+    acc = acc + val
+
+print(acc)
